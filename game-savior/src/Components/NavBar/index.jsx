9023a3f7ffc1371e1b37bf7e43a523d14126730e
Guilherme Silva <guilhermemas2004@gmail.com>
2023-11-09 11:25:31 +0000

    navbar fix

--- game-savior/src/Components/NavBar/index.jsx
+++ game-savior/src/Components/NavBar/index.jsx
@@ -1,34 +1,28 @@
 import { Link } from "react-router-dom";
-import "./style.css"
+import "./style.css";
 import React from "react";
-import { ReactComponent as logo } from "../../assets/Images/GameSavior.svg";
-import { ReactComponent as cart } from "../../assets/Images/cart-logo.svg";
+import logo from "../../assets/Images/GameSavior.png";
+import cart from "../../assets/Images/cart-logo.png";
 
-export default function NavBar(){
-    return(
-        <div className="navbar">
-        <div className="left-section">
-          <Link to="/" className="link">
-            <img src={logo} alt="logo" className="logo"/>
-          </Link>
-        </div>
-      
-        <div className="links">
-          <Link to="/alltickets" className="link">
-            All Tickets
-          </Link>
-        </div>
-      
-        <div className="right-section">
-          <Link to="/cart" className="link">
-            <img src={cart} alt="Cart" className="cart-icon"/>
-          </Link>
-        </div>
+export default function NavBar() {
+  return (
+    <div className="navbar">
+      <div className="left-section">
+        <Link to="/" className="link">
+            <img src={logo} alt="Logo" className="logo" />        </Link>
       </div>
-      
+
+      <div className="links">
+        <Link to="/alltickets" className="link">
+          All Tickets
+        </Link>
+      </div>
+
+      <div className="right-section">
+        <Link to="/cart" className="link">
+          <img src={cart} alt="Cart" className="cart-icon" />
+        </Link>
+      </div>
+    </div>
   );
-};
-
-
-
-
+}
