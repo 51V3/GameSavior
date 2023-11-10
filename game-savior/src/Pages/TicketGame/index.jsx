e81928ec256e8c635c8f9f18a669370697bbe650
Guilherme Silva <guilhermemas2004@gmail.com>
2023-11-10 11:44:44 +0000

    testing new api

--- game-savior/src/Pages/TicketGame/index.jsx
+++ game-savior/src/Pages/TicketGame/index.jsx
@@ -2,13 +2,12 @@ import axios from "axios"
 
 export default function TicketGame(){
 
-    axios.defaults.headers['X-Auth-Token'] = '11476ea25dc240e3896d3c993c233c5f';
-        axios.get('https://api.football-data.org/v2/matches').then((response)=>{
-        })
-        .catch((error)=>{
-            console.log(error);
-        })
-
+    axios.get('https://balldontlie.io/api/v1/games?seasons[]=2023').then((response)=>{
+        console.log(response);
+    })
+    .catch((error)=>{
+        console.log(error);
+    })
 
     return(
         <div>
