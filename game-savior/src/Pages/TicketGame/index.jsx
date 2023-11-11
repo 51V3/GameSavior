4ddb9abae241fa27ebcf8b646760dbf0bf266175
Caroline Vallim <carolinecvallim@gmail.com>
2023-11-11 13:24:18 +0000

    vite.config update

--- game-savior/src/Pages/TicketGame/index.jsx
+++ game-savior/src/Pages/TicketGame/index.jsx
@@ -1,48 +1,17 @@
-import React, { useEffect, useState } from 'react';
-const MatchesComponent = () => {
-    const [matches, setMatches] = useState([]);
-    useEffect(() => {
-        fetchMatches();
-    }, []);
-    
-    async function fetchMatches() {
-        try {
-          const response = await fetch('/api/matches');
-          // Check if the response is ok (status in the range 200-299)
-          if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-          }
-          // Check the content type of the response
-          const contentType = response.headers.get('content-type');
-          if (!contentType || !contentType.includes('application/json')) {
-            throw new Error('Oops, we haven\'t got JSON!');
-          }
-          const data = await response.json();
-          console.log("Fetched data:", data);
-          setMatches(data.matches);
-        } catch (error) {
-          console.error('Error fetching data:', error.message);
-        }
-      }
-return (
+import axios from "axios";
+
+export default function TicketGame() {
+  axios.get('/api/matches')
+  .then((response) => {
+    console.log(response);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+  return (
     <div>
-      <h1>Football Matches</h1>
-      <ul>
-        {matches.map(match => (
-          <li key={match.id}>
-            <h2>{match.competition.name} - Matchday {match.matchday}</h2>
-            <p>Date: {new Date(match.utcDate).toLocaleString()}</p>
-            <div>
-              <h3>{match.homeTeam.name} vs {match.awayTeam.name}</h3>
-              <img src={match.homeTeam.crest} alt={match.homeTeam.name} style={{ width: '50px' }} />
-              <span> VS </span>
-              <img src={match.awayTeam.crest} alt={match.awayTeam.name} style={{ width: '50px' }} />
-            </div>
-            <p>Status: {match.status}</p>
-          </li>
-        ))}
-      </ul>
+      {/* Your component content */}
     </div>
   );
-        }
-export default MatchesComponent
+}
