27f0ec564c8cc94f357ad49e231e7e219d4d8276
Caroline Vallim <carolinecvallim@gmail.com>
2023-11-13 10:03:54 +0000

    clean the file

--- game-savior/src/Pages/SingleTicket/index.jsx
+++ game-savior/src/Pages/SingleTicket/index.jsx
@@ -1,5 +1,3 @@
-// SingleTicket.js
-
 import axios from "axios";
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
@@ -24,10 +22,15 @@ export default function SingleTicket() {
       <h2>Game Details</h2>
       {match && (
         <div>
+          <div className="competition-details">
+            
+          </div>
+          <div className="game-details">
+          </div>
           <p>Game ID: {match.id}</p>
           <p>Team 1: {match.homeTeam.name}</p>
           <p>Team 2: {match.awayTeam.name}</p>
-          {/* Display other details as needed */}
+          
         </div>
       )}
     </div>
--- game-savior/src/Pages/TicketGame/index.jsx
+++ game-savior/src/Pages/TicketGame/index.jsx
@@ -1,5 +1,3 @@
-// TicketGame.js
-
 import axios from "axios";
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
