998aa99423c5901e65989b6b0d5feb393abce926
Guilherme Silva <guilhermemas2004@gmail.com>
2023-11-16 10:01:46 +0000

    cart styling

--- game-savior/src/Pages/CartPage/index.jsx
+++ game-savior/src/Pages/CartPage/index.jsx
@@ -51,7 +51,7 @@ export default function Cart() {
   };
 
   if (loading) {
-    return <p>Loading...</p>;
+    return <img src="https://www.gifcen.com/wp-content/uploads/2021/01/rolling-football-ball-gif.gif"/>;
   }
 
   if (error) {
